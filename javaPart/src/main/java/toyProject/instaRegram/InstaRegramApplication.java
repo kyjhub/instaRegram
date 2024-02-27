872d6ff30367b47113455ec891bcaffb77c3f804
Yong-jun Kwon <harryjun43@gmail.com>
2024-02-27 11:40:28 +0900

    java start

---
+++ javaPart/src/main/java/toyProject/instaRegram/InstaRegramApplication.java
@@ -0,0 +1,13 @@
+package toyProject.instaRegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaRegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaRegramApplication.class, args);
+	}
+
+}
---
+++ javaPart/src/test/java/toyProject/instaRegram/InstaRegramApplicationTests.java
@@ -0,0 +1,13 @@
+package toyProject.instaRegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaRegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
